fc0ec4e4f3f1c55b98a0123ac138d73ad9961b35
Marcelo Rodrigues <marques.marcelo.rodrigues@gmail.com>
2025-06-13 11:21:51 -0300

    feat: Lista de Preferências dos alunos testado

---
+++ src/preparacao_dados.py
@@ -0,0 +1,34 @@
+
+
+import pandas as pd
+from carregamento_dados import carregar_data, CAMINHO_PROJETOS, CAMINHO_ALUNOS #teste
+
+def preparar_listas_preferencia( df_projetos , df_alunos ):  # validacao das preferencias dos alunos
+   
+    prefs_validadas = []
+
+    for a_id, aluno in df_alunos.iterrows():
+        
+        nota_aluno = aluno ['nota']
+        prefs_validadas_aluno = []
+
+        for proj_id in aluno['preferencia']:
+            
+            if proj_id in df_projetos.index:
+
+                nota_minima_proj = df_projetos.loc[proj_id, 'nota_minima']
+
+                if nota_aluno >= nota_minima_proj:
+
+                    prefs_validadas_aluno.append(proj_id)
+
+        prefs_validadas.append(prefs_validadas_aluno)
+        print(f"{a_id}: {prefs_validadas_aluno}")
+
+    df_alunos ['prefs_validadas'] = prefs_validadas
+
+    print(f" preferências validadas para o aluno 200  {a_id}: {prefs_validadas_aluno}") #ultimo aluno
+
+
+preparar_listas_preferencia( carregar_data( CAMINHO_PROJETOS , CAMINHO_ALUNOS )[0] , carregar_data( CAMINHO_PROJETOS , CAMINHO_ALUNOS )[1] )
+
