c81787e16cd32932542538c68b5484533bc5a0e0
Marcelo Rodrigues <marques.marcelo.rodrigues@gmail.com>
2025-06-13 23:38:01 -0300

    chore: código refatorado black/flaske8 e comentários

--- src/carregamento_dados.py
+++ src/carregamento_dados.py
@@ -1,44 +1,54 @@
 import pandas as pd
 import re
 
-CAMINHO_PROJETOS = 'data/projetodata.txt'
-CAMINHO_ALUNOS = 'data/alunodata.txt'
+CAMINHO_PROJETOS = "data/projetodata.txt"
+CAMINHO_ALUNOS = "data/alunodata.txt"
 
-def carregar_data( CAMINHO_PROJETOS , CAMINHO_ALUNOS ):
+
+def carregar_data(CAMINHO_PROJETOS, CAMINHO_ALUNOS):
     try:
-        with open(CAMINHO_PROJETOS, 'r', encoding='utf-8') as arquivo:
-            linhas_projetos = arquivo.readlines()
+        with open(CAMINHO_PROJETOS, "r", encoding="utf-8") as arquivo:
+            linhas_projetos = arquivo.readlines()  # Carregamento de projetos
 
         dados_projetos = []
-        padrao_projeto = re.compile(r'\(P(\d+),\s*(\d+),\s*(\d+)\)')  # id, vagas , nota minima
+        padrao_projeto = re.compile(
+            r"\(P(\d+),\s*(\d+),\s*(\d+)\)"
+        )  # Captura (P<id>, <vagas>, <nota_min>)
 
         for linha in linhas_projetos:
             match = padrao_projeto.search(linha)
             if match:
                 pid, vagas, nota_minima = match.groups()
-                dados_projetos.append([f"P{pid}", int(vagas), float(nota_minima)])
+                dados_projetos.append([f"P{pid}",
+                                       int(vagas),
+                                       float(nota_minima)])
 
-        df_projetos = pd.DataFrame(dados_projetos, columns=['id', 'vagas', 'nota_minima'])
-        df_projetos = df_projetos.set_index('id')
+        df_projetos = pd.DataFrame(
+            dados_projetos, columns=["id", "vagas", "nota_minima"]
+        )
+        df_projetos = df_projetos.set_index("id")
 
-        with open(CAMINHO_ALUNOS, 'r', encoding='utf-8') as arquivo:
-            linhas_alunos = arquivo.readlines()
+        with open(CAMINHO_ALUNOS, "r", encoding="utf-8") as arquivo:
+            linhas_alunos = arquivo.readlines()  # Carregamento de alunos
 
             dados_alunos = []
-            padrao_aluno = re.compile(r'\(A(\d+)\):\((.*?)\)\s*\((\d+)\)')  # (id) :  preferencia , nota
+            padrao_aluno = re.compile(
+                r"\(A(\d+)\):\((.*?)\)\s*\((\d+)\)"
+            )  # Capturamos (A<id>):(<preferencias>) (<nota>)
 
         for linha in linhas_alunos:
             match = padrao_aluno.search(linha)
             if match:
                 aid, preferencia, nota = match.groups()
-                preferencias = [p.strip() for p in preferencia.split(',')]
-                dados_alunos.append([f"A{aid}", preferencias, int(nota)]) ##
-        
-        df_alunos = pd.DataFrame(dados_alunos, columns=['id', 'preferencia', 'nota'])
-        df_alunos = df_alunos.set_index('id')
+                # Trata a string das prefeências pra lista
+                preferencias = [p.strip() for p in preferencia.split(",")]
+                dados_alunos.append([f"A{aid}", preferencias, int(nota)])
+
+        df_alunos = pd.DataFrame(dados_alunos, columns=["id", "preferencia", "nota"])
+        df_alunos = df_alunos.set_index("id")
 
         return df_projetos, df_alunos
-    
+
     except FileNotFoundError as e:
         print(f"ERRO de arquivo não encontrado no caminho: {e.filename}")
-        return None , None
+        return None, None
--- src/preparacao_dados.py
+++ src/preparacao_dados.py
@@ -1,65 +1,75 @@
 import pandas as pd
-from carregamento_dados import carregar_data, CAMINHO_PROJETOS, CAMINHO_ALUNOS #teste
+from carregamento_dados import carregar_data
+from carregamento_dados import CAMINHO_PROJETOS, CAMINHO_ALUNOS
 
-def preparar_listas_preferencia( df_projetos , df_alunos ):  # validacao das preferencias dos alunos
-   
+
+def preparar_listas_preferencia(
+    df_projetos, df_alunos
+):
+
     prefs_validadas = []
-
+    # Itera todos para catar preferências
     for a_id, aluno in df_alunos.iterrows():
-        
-        nota_aluno = aluno ['nota']
+
+        nota_aluno = aluno["nota"]
         prefs_validadas_aluno = []
 
-        for proj_id in aluno['preferencia']:
-            
+        for proj_id in aluno["preferencia"]:
+
             if proj_id in df_projetos.index:
 
-                nota_minima_proj = df_projetos.loc[proj_id, 'nota_minima']
-
+                nota_minima_proj = df_projetos.loc[proj_id, "nota_minima"]
+                # Aluno deve ter nota suficiente
                 if nota_aluno >= nota_minima_proj:
 
                     prefs_validadas_aluno.append(proj_id)
 
         prefs_validadas.append(prefs_validadas_aluno)
-        """print(f"{a_id}: {prefs_validadas_aluno}")"""
+    # Nova lista de preferências como uma nova coluna
+    df_alunos["prefs_validadas"] = prefs_validadas
 
-    df_alunos ['prefs_validadas'] = prefs_validadas
 
-    """print(f" preferências validadas para o aluno 200  {a_id}: {prefs_validadas_aluno}") #ultimo aluno"""
 """
+
+
 df_projetos, df_alunos = carregar_data(CAMINHO_PROJETOS, CAMINHO_ALUNOS)
-preparar_listas_preferencia(df_projetos, df_alunos) """
+preparar_listas_preferencia(df_projetos, df_alunos)
 
-def criar_preferencias_projetos( df_projetos , df_alunos ):
 
-    candidatos_por_projeto = {proj_id:[] for proj_id in df_projetos.index}
+"""
 
-    for aluno_id , aluno_data in df_alunos.iterrows():
-        
-        for proj_id in aluno_data ['prefs_validadas']:
-            
-            candidatos_por_projeto[proj_id].append({ 'id' : aluno_id , 'nota' : aluno_data['nota'] })
 
+def criar_preferencias_projetos(df_projetos, df_alunos):
+    # Todos os candidatos para cada projeto
+    candidatos_por_projeto = {proj_id: [] for proj_id in df_projetos.index}
+
+    for aluno_id, aluno_data in df_alunos.iterrows():
+
+        for proj_id in aluno_data["prefs_validadas"]:
+
+            candidatos_por_projeto[proj_id].append(
+                {"id": aluno_id, "nota": aluno_data["nota"]}
+            )
+    # Ordena candidatos por projeto com base na nota
     dict_projetos_final = {}
 
     for proj_id, candidatos in candidatos_por_projeto.items():
+        # Ordena decescente
+        candidatos.sort(key=lambda x: x["nota"], reverse=True)
+        # Retira os IDs, aggora em ordem.
+        dict_projetos_final[proj_id]=[candidato["id"] for candidato in candidatos]
 
-        candidatos.sort( key = lambda x: x['nota'] , reverse=True )
-
-        dict_projetos_final[proj_id] = [ candidato['id'] for candidato in candidatos ]
-
-    df_projetos ['preferencia'] = pd.Series(dict_projetos_final)  # isso adiciona nova coluna no datafra,e
-
-    """print('passou por aqui')"""
+    df_projetos["preferencia"] = pd.Series(
+        dict_projetos_final
+    )  # Adiciona a lista de preferências como uma nova coluna
 
     return df_projetos
-    
-### teste ###
 
-if __name__ == "__main__":
-    from carregamento_dados import carregar_data, CAMINHO_PROJETOS, CAMINHO_ALUNOS
 
-    print('--'*20)
+"""
+if __name__ == "__main__":  # Teste do modulo, tirar depois de pronto
+
+    print("--" * 20)
 
     df_projetos, df_alunos = carregar_data(CAMINHO_PROJETOS, CAMINHO_ALUNOS)
 
@@ -67,14 +77,17 @@ if __name__ == "__main__":
 
     df_projetos = criar_preferencias_projetos(df_projetos, df_alunos)
 
-    print('--'*8 + 'Carregou' + '--'*8)
+    print("--" * 8 + "Carregou" + "--" * 8)
 
-    
-    print(df_alunos.loc['A1'][[ 'nota' , 'prefs_validadas' ]])
-    print(df_alunos.loc['A77'][[ 'nota' , 'prefs_validadas' ]])  # aluno 77 com suas preferencias validadas
+    print(df_alunos.loc["A1"][["nota", "prefs_validadas"]])
+    print(
+        df_alunos.loc["A77"][["nota", "prefs_validadas"]]
+    )  # aluno 77 com suas preferencias validadas
 
-    print(df_projetos.loc['P1'][['vagas', 'nota_minima', 'preferencia']])  # projeto 1 com suas preferencias
-    print(df_projetos.loc['P37'][['vagas', 'nota_minima', 'preferencia']])  # 2
+    print(
+        df_projetos.loc["P1"][["vagas", "nota_minima", "preferencia"]]
+    )  # projeto 1 com suas preferencias
+    print(df_projetos.loc["P37"][["vagas", "nota_minima", "preferencia"]])  # 2
 
-    pd.set_option('display.max_seq_items', None)
-    print(df_projetos.loc['P37']['preferencia'])
+    pd.set_option("display.max_seq_items", None)
+    print(df_projetos.loc["P37"]["preferencia"]) """
